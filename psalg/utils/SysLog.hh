8689dbf37fa11dfa3fe777432ad986e923c30cfe
Chris Ford <caf@slac.stanford.edu>
2019-10-29 15:55:42 -0700

    SysLog C++ client moved to psalg/utils.

---
+++ psalg/utils/SysLog.hh
@@ -0,0 +1,26 @@
+#ifndef PDS_SYSLOG_HH
+#define PDS_SYSLOG_HH
+
+#include <syslog.h>     // defines LOG_WARNING, etc
+
+#define SYSLOG_IDENT_MAX    32
+#define SYSLOG_FORMAT_MAX   4096
+
+namespace psalg {
+class SysLog
+{
+public:
+    // initialize
+    static void init(const char *instrument, int level);
+
+    // log
+    static void debug(const char *fmt, ...);
+    static void info(const char *fmt, ...);
+    static void warning(const char *fmt, ...);
+    static void error(const char *fmt, ...);
+    static void critical(const char *fmt, ...);
+};
+}
+
+#endif
+
